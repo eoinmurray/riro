9464ab720a9804f04feb9d83b9d343f48f5c0e87
eoinmurray <amonecho1@gmail.com>
2016-07-21 22:47:44 +0100

    initial

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+var path = require('path')
+var fs = require('fs')
+var cwd = process.cwd()
+
+function getUserHome() {
+  return process.env.HOME || process.env.USERPROFILE;
+}
+
+var bashProfile = path.join(getUserHome(), '.bash_profile')
+
+fs.exists(bashProfile, function(exists){
+  if (!exists) return console.log(`Sorry ${bashProfile} does not exist.`)
+
+  fs.readFile(bashProfile, function(err, data){
+    if (err) throw err;
+    fs.writeFile(bashProfile + '.riro', data, function(err){
+      if (err) throw err;
+
+      var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(bashProfile)
+      });
+
+      var lines = ''
+      var found = false
+      var newAlias = `alias p= 'cd ${cwd}'\n`
+      lineReader.on('line', function (line) {
+        if (line.startsWith('alias p=')){
+          lines += newAlias
+          found = true
+        } else {
+          lines += line + '\n'
+        }
+      })
+
+      lineReader.on('close', function(){
+        if (!found) lines += newAlias
+
+        fs.writeFile(bashProfile, lines, function(err){
+          if (err) throw err;
+          console.log(`set alias "p" to ${cwd} in ${bashProfile}`)
+        })
+      })
+    })
+  })
+})
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+
+# riro
+
+Aliases your current working directory to `p` so you can jump to your current
+project every time you open a terminal.
+
+Just run
+
+```
+riro
+>>> set alias "p" to /Users/eoinmurray/Dropbox/macbook-pro/riro in /Users/eoinmurray/.bash_profile
+```
+
+It also makes a copies your .bash_profile beforehand into .bash_profile.riro for safekeeping.
+
+Change project? Just run `riro` again!
